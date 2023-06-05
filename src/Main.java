cc312e857bf173b1bc2c35d218d273abafa0c921
veronikaostrovski <veronika.turkey@gmail.com>
2023-06-06 01:30:08 +0300

    Task 1 done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+    // Task1
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int[] myArray = {1, 4, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 0; i < myArray.length; i++) {
+            for (int e = i + 1; e < myArray.length; e++) {
+                System.out.println("Повторяющихся элементов нет");
+            }
+        }
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
         }
     }
-}
+
